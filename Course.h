2755821e41beeb9e217a236fdb6550be94f3f972
SalmaRostom-cs16001646 <salmarostom277@gmail.com>
2025-11-11 09:28:10 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,19 +4,16 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Course{
+    private:
+        string courseCode;
+        string courseName;
+        int maxStudents;
+        Student* students;
+        int currentStudents;
+    public:
+        void Course(string c="",sting n="",int m=0,Student* s=NULL,int cs=0);
+        void addStudent(const Student& s);
+        void displayCourseInfo();
+};
 #endif
